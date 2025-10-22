10df71470a131aace825b6900243e3d6fa2dcd1f
Manuel Felipe Barrera Barrera <felipebarrera104@gmail.com>
2025-10-22 10:14:48 -0500

    Add files via upload

---
+++ src/test/java/com/example/parcialarep/ParcialarepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parcialarep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialarepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
