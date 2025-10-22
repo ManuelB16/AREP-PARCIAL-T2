f583f321231e1c23aaa69ca2c83f52b9425e6ba5
Manuel Felipe Barrera Barrera <felipebarrera104@gmail.com>
2025-10-22 10:38:14 -0500

    Fixed Lucas

---
+++ src/main/java/com/example/parcialarep/LucasController.java
@@ -0,0 +1,41 @@
+package com.example.parcialarep;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LucasController {
+
+    @GetMapping("/lucasseq")
+    public LucasResponse lucasSequence(@RequestParam int value) {
+        String sequence = computeLucasSequence(value);
+        return new LucasResponse("Secuencia de Lucas", value, sequence);
+    }
+
+    private String computeLucasSequence(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Value must be non-negative");
+        }
+        if (n == 0) {
+            return "2";
+        }
+        if (n == 1) {
+            return "2, 1";
+        }
+        long[] lucas = new long[n + 1];
+        lucas[0] = 2;
+        lucas[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            lucas[i] = lucas[i - 1] + lucas[i - 2];
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= n; i++) {
+            if (i > 0) sb.append(", ");
+            sb.append(lucas[i]);
+        }
+        return sb.toString();
+    }
+}
+
+record LucasResponse(String operation, int input, String output) {}
